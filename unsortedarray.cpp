5e281ba96f3e493ab27018b830cb6166f01af631
BVPVNarasimhaMurty <84016456+BVPVNarasimhaMurty@users.noreply.github.com>
2021-05-13 20:17:52 +0530

    Update unsortedarray.cpp

--- unsortedarray.cpp
+++ unsortedarray.cpp
@@ -1,31 +1,34 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-int main()
+import java.util.*;
+class LongestArray
 {
-	int n,l,r;
-	cin>>n;
-	l=n,r=0;
-	int arr[n],s[n];
-	for(int i=0;i<n;i++)
+	public static void main(String args[])
 	{
-		cin>>arr[i];
-		s[i]=arr[i];
-	}
-     sort(s,s+n);	 
-	 for(int i=0;i<n;i++)
-	 {
-	 	if(arr[i]!=s[i])
+		Scanner sc=new Scanner(System.in);
+		int n,l,r;
+		n=sc.nextInt();
+		l=n;r=0;
+		int arr[]=new int[n];
+		int s[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			s[i]=arr[i];
+		}
+		Arrays.sort(s);	 
+	 	for(int i=0;i<n;i++)
 	 	{
-	 		if(i<l)
-	 		l=i;
-	 		if(i>r)
-	 		r=i;
-		 }
-	  } 
-	 if(r-l>=0)
-	  cout<<r-l+1;
-	   else
-	   cout<<"0";
-	 return 0;
+	 		if(arr[i]!=s[i])
+	 		{
+	 			if(i<l)
+	 			l=i;
+	 			if(i>r)
+	 			r=i;
+		 	}
+	  	}	 
+	 	if(r-l>=0)
+	  		System.out.println(r-l+1);
+		else
+	   		System.out.println("0");
+
+	}
 }
